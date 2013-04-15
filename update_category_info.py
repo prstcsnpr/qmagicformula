d4e70ee695912aa9b686ac0fe6a9e167838aea7a
prstcsnpr <prstcsnpr@192.168.1.102>
2013-04-15 21:59:59 +0800

    将分类数据写下数据库

--- update_category_info.py
+++ update_category_info.py
@@ -1,58 +1,25 @@
 #coding=utf8
 
 
-import datetime
-import json
 import logging
-import re
 import string
-import sys
-import urllib
-from google.appengine.api.labs import taskqueue
-from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-import exchange_rate
 import stock
 
 
 class UpdateCategoryInfoHandler(webapp.RequestHandler):
 
     def get(self):
-        taskqueue.add(url='/tasks/updateallcategoryinfo',
-                      queue_name='updateallcategoryinfo',
-                      method='GET')
-        
-
-class UpdateAllCategoryInfoHandler(webapp.RequestHandler):
-    
-    def get(self):
-        url = 'http://stockapp.finance.qq.com/mstats/menu_childs.php?id=bd_csrc'
-        result = urlfetch.fetch(url=url)
-        if 200 == result.status_code:
-            data = result.content.decode('GBK').encode('UTF-8')
-            data = json.loads(data)
-            for i in data['bd_csrc']['chd']:
-                taskqueue.add(url='/tasks/updatesinglecategoryinfo',
-                              queue_name='updatesinglecategoryinfo',
-                              method='GET',
-                              params={'category' : i})
-                
-                
-class UpdateSingleCategoryInfoHandler(webapp.RequestHandler):
-    
-    def get(self):
-        category = self.request.get('category')
-        url = 'http://stock.gtimg.cn/data/get_hs_xls.php?id=pt' + category[2:] + '&type=1&metric=name'
-        result = urlfetch.fetch(url=url)
-        if 200 == result.status_code:
-            data = result.content
-            print data
+        with open('category') as file:
+            for line in file.readlines():
+                fields = line.split()
+                entry = stock.get(fields[0])
+                entry.categories.append(fields[1])
+                stock.put(fields[0], entry)
         
         
-application = webapp.WSGIApplication([('/tasks/updatecategoryinfo', UpdateCategoryInfoHandler),
-                                      ('/tasks/updateallcategoryinfo', UpdateAllCategoryInfoHandler),
-                                      ('/tasks/updatesinglecategoryinfo', UpdateSingleCategoryInfoHandler)],
+application = webapp.WSGIApplication([('/tasks/updatecategoryinfo', UpdateCategoryInfoHandler)],
                                      debug=True)
 
 
