ed6688fc248b6385510401c659c9fc0946756d42
prstcsnpr <prstcsnpr@10.2.201.237>
2014-05-12 18:04:51 +0800

    增加BAE

---
+++ bae.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+import httplib
+import logging
+import string
+from google.appengine.api.labs import taskqueue
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+import show_stock_info
+
+class BAEHandler(webapp.RequestHandler):
+    def get(self):
+        entry = show_stock_info.get('magicformulaforjson')
+        logging.info(entry.content)
+        uri = ''
+        with open('config/uri') as file:
+            for line in file.readlines():
+                if len(line) > 0:
+                    uri = line.strip()
+                    break
+        c = httplib.HTTPConnection('bcs.duapp.com')
+        c.request("PUT", uri, entry.content)
+        r = c.getresponse()
+        
+application = webapp.WSGIApplication([('/tasks/bae', BAEHandler)], debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+    
+    
+if __name__ == '__main__':
+    main()
