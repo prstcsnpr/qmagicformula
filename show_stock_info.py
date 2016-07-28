619c8c0a29826b88865728e8e7a524ac455987dc
ning.zhao <ning.zhao@laikanxing.com>
2016-07-28 13:50:43 +0800

    只取50

--- show_stock_info.py
+++ show_stock_info.py
@@ -289,7 +289,7 @@ class MagicFormulaHandler(webapp.RequestHandler):
         stocks = query.fetch(10000)
         stocks, pb, pe, roe, mc_gdp = self.__filter(stocks)
         stocks = self.__magicformula(stocks)
-        position = 100
+        position = 50
         while position<len(stocks):
             if stocks[position].rank == stocks[position - 1].rank:
                 position = position + 1
